41f2da612aced0cf14c5bc475631ec9309e0db8a
koo3751 <koo3751@naver.com>
2018-11-26 17:12:46 +0900

    11.25 / 로고 수정, 오프닝 이벤트 삽입 11.26 / 알람 설정 코드 삽입 11.26 / 메뉴 코드 삽입(껍데기만)

---
+++ app/src/main/java/com/yhsnmt/nmt/menu.java
@@ -0,0 +1,93 @@
+package com.yhsnmt.nmt;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ToggleButton;
+
+public class MenuActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        final ToggleButton tb1=findViewById(R.id.tb1);
+        final ToggleButton tb2=findViewById(R.id.tb2);
+        final Button route=findViewById(R.id.route);
+
+
+        tb1.setOnClickListener(new View.OnClickListener() {
+
+
+
+            @Override
+
+            public void onClick(View v) {
+
+                if(tb1.isChecked()){
+
+                    tb1.setBackgroundDrawable(
+
+                            getResources().
+
+                                    getDrawable(R.drawable.makon)
+
+                    );
+
+                }else{
+
+                    tb1.setBackgroundDrawable(
+
+                            getResources().
+
+                                    getDrawable(R.drawable.makoff)
+
+                    );
+
+                }
+
+            }
+
+        });
+
+        tb2.setOnClickListener(new View.OnClickListener() {
+
+
+
+            @Override
+
+            public void onClick(View v) {
+
+                if(tb2.isChecked()){
+
+                    tb2.setBackgroundDrawable(
+
+                            getResources().
+
+                                    getDrawable(R.drawable.haon)
+
+                    );
+
+                }else{
+
+                    tb2.setBackgroundDrawable(
+
+                            getResources().
+
+                                    getDrawable(R.drawable.haoff)
+
+                    );
+
+                }
+
+            }
+
+        });
+
+
+
+
+    }
+}
